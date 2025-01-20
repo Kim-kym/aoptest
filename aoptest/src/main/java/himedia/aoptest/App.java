3a0a5ebea95a1629df303b774ccfc1bd16c0c384
Kim-kym <msgim725@gmail.com>
2025-01-20 16:58:49 +0900

    Spring AOP

---
+++ aoptest/src/main/java/himedia/aoptest/App.java
@@ -0,0 +1,20 @@
+package himedia.aoptest;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class App {
+
+	public static void main(String[] args) {
+		ApplicationContext ac =
+				new ClassPathXmlApplicationContext("config/applicationContext.xml");
+		//	Spring Context로부터 ProductService Bean 획득 
+		ProductService ps =
+				ac.getBean(ProductService.class);
+		
+//		ProductVo vo = ps.findProduct("Camera");
+		ProductVo vo = ps.findProduct("bomb");
+		System.out.println(vo);
+	}
+
+}
---
+++ aoptest/src/main/java/himedia/aoptest/ProductService.java
@@ -0,0 +1,16 @@
+package himedia.aoptest;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class ProductService {
+	public ProductVo findProduct(String keyword) {
+		System.out.println("finding [" + 
+				keyword + "] ...");
+		if (keyword.equals("bomb")) {
+			throw new RuntimeException("Boom!");
+		}
+		return new ProductVo(keyword);
+	}
+	
+}
